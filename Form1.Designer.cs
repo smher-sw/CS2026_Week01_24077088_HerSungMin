f6ce4f0eb181aa411882b1dc2914a8d611df4a5b
gram <gram@LG-GRAM>
2026-02-12 12:55:18 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            label1.Location = new Point(215, 112);
+            label1.Name = "label1";
+            label1.Size = new Size(128, 48);
+            label1.TabIndex = 1;
+            label1.Text = "허성민";
+            label1.Click += label1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.허성민_얼굴사진;
+            pictureBox1.Location = new Point(34, 123);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(142, 187);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("맑은 고딕", 12F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            label2.Location = new Point(215, 186);
+            label2.Name = "label2";
+            label2.Size = new Size(411, 96);
+            label2.TabIndex = 3;
+            label2.Text = "수원대 지능형SW융합대학 겸임교수\r\n010-8950-9350 smher68@gmail.com\r\n서울시 강남구 대치동\r\n";
+            label2.Click += label2_Click;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("맑은 고딕", 14F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            button1.ForeColor = SystemColors.Highlight;
+            button1.Location = new Point(373, 368);
+            button1.Name = "button1";
+            button1.Size = new Size(216, 56);
+            button1.TabIndex = 4;
+            button1.Text = "배경색 바꾸기";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "나의 첫 GUI 프로그램";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Button button1;
+    }
+}
